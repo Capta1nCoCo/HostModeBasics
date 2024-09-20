cd15f4ef34a16ba301c3c4c8ff85827c8cbc4444
Capta1nCoCo <femabu28@gmail.com>
2024-09-20 13:01:28 +0200

    Physics(Refactoring): Ball(Base), KinematicBall, PhysxBall, Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,10 @@ public class Player : NetworkBehaviour
 {
     [SerializeField] private float _movementSpeed = 5.0f;
     [SerializeField] private float _spawnDelayInSeconds = 0.5f;
-    [SerializeField] private Ball _prefabBall;
+    [SerializeField] private KinematicBall _prefabKinematicBall;
     [SerializeField] private PhysxBall _prefabPhysxBall;
 
-    [Networked] private TickTimer _delay { get; set; }
+    [Networked] private TickTimer delayTimer { get; set; }
 
     private Vector3 _forward;
     private NetworkCharacterController _cc;
@@ -25,46 +25,46 @@ public class Player : NetworkBehaviour
         {
             data.direction.Normalize();
             _cc.Move(_movementSpeed * data.direction * Runner.DeltaTime);
-            ChangeForwardDirection(data);
-            SpawnBallForward(data);
+            CheckForwardDirection(data);
+            LaunchBallByInput(data);
         }
     }
 
-    private void ChangeForwardDirection(NetworkInputData data)
+    private void CheckForwardDirection(NetworkInputData data)
     {
         if (data.direction.sqrMagnitude > 0)
             _forward = data.direction;
     }
 
-    private void SpawnBallForward(NetworkInputData data)
+    private void LaunchBallByInput(NetworkInputData data)
     {
-        if (HasStateAuthority && _delay.ExpiredOrNotRunning(Runner))
+        if (HasStateAuthority && delayTimer.ExpiredOrNotRunning(Runner))
         {
             if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON0))
             {
-                _delay = TickTimer.CreateFromSeconds(Runner, _spawnDelayInSeconds);
-                Runner.Spawn(_prefabBall,
-                    transform.position + _forward, Quaternion.LookRotation(_forward),
-                    Object.InputAuthority, 
-                    (runner, obj) =>
-                    {
-                        // Initialize the Ball before synchronizing it
-                        obj.GetComponent<Ball>().Init();
-                    });
+                SpawnBall(_prefabKinematicBall);
             }
             else if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON1))
             {
-                _delay = TickTimer.CreateFromSeconds(Runner, _spawnDelayInSeconds);
-                Runner.Spawn(_prefabPhysxBall,
-                    transform.position + _forward,
-                    Quaternion.LookRotation(_forward),
-                    Object.InputAuthority,
-                    (runner, obj) =>
-                    {
-                        const float PBALL_VELOCITY = 10.0f;
-                        obj.GetComponent<PhysxBall>().Init(PBALL_VELOCITY * _forward);
-                    });
+                SpawnBall(_prefabPhysxBall);
             }
         }
+    }
+
+    private void SpawnBall(Ball prefabBall)
+    {
+        delayTimer = TickTimer.CreateFromSeconds(Runner, _spawnDelayInSeconds);
+        Runner.Spawn(prefabBall,
+            transform.position + _forward, Quaternion.LookRotation(_forward),
+            Object.InputAuthority,
+            InitBallBeforeSync());
+    }
+
+    private NetworkRunner.OnBeforeSpawned InitBallBeforeSync()
+    {
+        return (runner, obj) =>
+        {
+            obj.GetComponent<Ball>().Init();
+        };
     }
 }
